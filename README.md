9f19b0db17cae78788a1d6f9841b793a6c0f9b6c
chris.ridgway.nz@gmail.com <chris.ridgway.nz@gmail.com>
2021-07-01 21:30:20 +1200

    Initial Push

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # node_discord_bridge
 Node.js standalone app for Natural Selection 2 &lt;-> Discord communication
+
+##\#1 Create Discord Bot Token
+https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token
+
+##\#2 Get Channel Id
+The bot requires a channel to send to and read messages from.
+
+Enable Developer Mode. User Settings -> Advanced.
+Right click a channel in Discord -> Copy ID.
+
+##\#3 Clone repository
+Ensure node.js is installed.
+
+`git clone https://github.com/Turbine1991/node-discord-bridge.git`
+
+Go inside directory.
+`npm install`
+
+##\#4 Configure config.json
+```
+{
+  "discord_token": "PUT YOUR TOKEN HERE",
+  "discord_channel_ids": {
+    "server_1": "PUT YOUR CHANNEL ID HERE"
+  },
+  "listen_path": "/discordbridge",
+  "listen_port": 3100
+}
+```
+
+##\#5 Run
+`node index`
---
+++ index.js
@@ -0,0 +1,103 @@
+//http://127.0.0.1:3100/discordbridge?server_id=server_1&game_messages=[%22abc%22,%22def%22]
+const config = require('./config.json');
+const Discord = require('discord.js');
+const discord = new Discord.Client();
+
+// Declare
+if (!config || !config.discord_token || !config.discord_channel_ids || !config.listen_path || !config.listen_port)
+  throw new Error("Please configure 'config.json'");
+
+let instances = null;
+
+function loadInstances() {
+  instances = Object.entries(config.discord_channel_ids).reduce((obj, [server_id, channel_id]) => {
+    ////console.log(discord.channels.cache.get(channel_id));
+    obj[server_id] = { channel_id, server_id, discord_queue: [], channel: discord.channels.cache.get(channel_id) }; // Server to channel lookup
+    obj[channel_id] = obj[server_id]; // Channel to server lookup
+    return obj;
+  }, {});
+}
+
+// Handle messages from Discord
+discord.on('ready', () => {
+  console.log('Connected to Discord');
+  loadInstances();
+  loadFastify();
+});
+
+discord.on('message', msg => {
+  console.log(msg.content);
+  instances[msg.channel.id]?.discord_queue.push({name: msg.author.username, msg: msg.content}); // Add message to channel queue, if it's in the config
+});
+
+discord.login(config.discord_token);
+
+// Handle messages from Game
+const fastify = require('fastify')();
+fastify.register(require('fastify-formbody'))
+
+function loadFastify() {
+  function route(request, reply) {
+    const body = request.body || request.query;
+    const server_id = body.server_id || '';
+    
+    console.log(Date.now());
+    const instance = instances[server_id];
+    if (!instance)
+      throw new Error(`ServerIdentifier from DiscordBridge.js '${server_id}' does not exist in 'config.json'`);
+
+    const discord_messages = instance.discord_queue;
+    instance.discord_queue = [];
+
+    // Bad naming from original code
+    if (body.msgs) {
+      body.msgs = JSON.parse(body.msgs);
+
+      for(const message of body.msgs) {
+        switch (message.type) {
+          case 'chat':
+            //let {name, steam_id, team, body} = message;
+            let teamName = '[Spec]';
+            if (message.team_id <= 1)
+              teamName = (message.team_id == 0)? '[Marine]': '[Alien]';
+            
+            instance.channel.send(`${teamName} ${message.name}: ${message.body}`);
+            break;
+          case 'player':
+            //let {name, steam_id, player_count, sub} = message;
+            instance.channel.send(`${message.name} has ${(message.sub === 'join')? 'joined': 'left'} the server (${message.player_count})`);
+            break;
+          case 'info':
+            //let {body, sub} = message;
+            break;
+          case 'status':
+            //let {body, sub, player_count} = message;
+            instance.channel.send(`${message.body} (${message.player_count})`);
+            break;
+          case 'adminprint':
+            //let {body} = message;
+            instance.channel.send(`${message.body}`);
+            break;
+        }
+      }
+    }
+
+    return merge_chat_return(discord_messages);
+  }
+
+  fastify.get(config.listen_path, route);
+  fastify.post(config.listen_path, route);
+
+  fastify.listen(config.listen_port, async (err, address) => {
+    if (err) throw new Error(err);
+    console.log(`Listening on '${address}${config.listen_path}'`)
+  })
+}
+
+function merge_chat_return(messages) {
+  result = '';
+  for(const message of messages)
+    result += `chat${message.name}${message.msg}`;
+
+  return result;
+}
